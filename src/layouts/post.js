4f0c03611eb7414b93b4ca72060e0957cbcbb210
Stackbit Code Editor <projects@stackbit.com>
2021-11-05 20:36:09 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -32,7 +32,7 @@ export default class Post extends React.Component {
                 </div>
                 {_.get(this.props, 'page.subtitle', null) && (
                   <div className="post-subtitle">asd
-                    {htmlToReact(_.get(this.props, 'page.sectionImageAndText50And50.displayField', null))}
+                    {htmlToReact(_.get(this.props, 'page.sectionImageAndText50And50.heading', null))}
                   qwe</div>
                   )}
               </article>
