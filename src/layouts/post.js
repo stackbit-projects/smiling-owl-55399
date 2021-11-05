a1b458a0c860e774d9eaa679dd4d38908b37c427
Stackbit Code Editor <projects@stackbit.com>
2021-11-05 20:10:18 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -30,7 +30,11 @@ export default class Post extends React.Component {
                 <div className="post-content inner-md">
                   {markdownify(_.get(this.props, 'page.content', null))}
                 </div>
-                
+                {_.get(this.props, 'page.subtitle', null) && (
+                  <div className="post-subtitle">
+                    {htmlToReact(_.get(this.props, 'page.subtitle', null))}
+                  </div>
+                  )}
               </article>
             </Layout>
         );
